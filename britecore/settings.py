115fb35291a770332de8c3c8ff05a4f24a00d390
Kinetic Alnajar <kalnajar09@gmail.com>
2018-03-06 04:37:07 -0500

    reference static in the index

--- britecore/settings.py
+++ britecore/settings.py
@@ -134,15 +134,9 @@ AWS_STORAGE_BUCKET_NAME = 'britecore-storage'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_ACCESS_KEY_ID = 'AKIAISZNFNSU2DT4H62Q'
 AWS_SECRET_ACCESS_KEY = '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'
-
-# Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
-# you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 STATICFILES_LOCATION = 'static'
 
